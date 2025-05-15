1b83fb92faa6938904158feda84a51116bee78c5
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-05-15 23:47:27 +0330

    Update App.jsx

--- smart-todo/src/App.jsx
+++ smart-todo/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="flex items-center justify-center h-screen bg-blue-500">
       <h1 className="text-white text-4xl font-bold">SmartTodo</h1>
     </div>
-  )
+  );
 }
 
 export default App;
