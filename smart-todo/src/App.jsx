9c8eac8bb122150990d6d3c86c5f4e93e6c60712
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-05-18 01:20:56 +0330

    Update App.jsx

--- smart-todo/src/App.jsx
+++ smart-todo/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import react, { useState } from 'react';
 function App() {
-  const [task, setTask] = useState('');
+  const [taskName, setTaskName] = useState('');
   return (
-    <div className='p-4 max-w-md mx-auto'>
-      <h1 className='text-2x1 font-bold mb-4'>Smart Todo</h1>
+    <div className='p-4 max-w-md mx-auto grid'>
+      <h1 className='mx-auto text-2x1 font-bold mb-4'>Smart Todo</h1>
+      <label className='mx-auto'>Task Name:</label>
+      <input type='text' id='task' className='mx-auto'/>
+      <label className='mx-auto'>Time:</label>
+      <input type='text' id='time' className='mx-auto'/>
     </div>
   );
 }
